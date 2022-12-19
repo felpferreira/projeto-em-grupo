6026086691c198e564835118185603a38473c20f
felpferreira <118365423+felpferreira@users.noreply.github.com>
2022-12-19 09:19:43 -0300

    Add files via upload

---
+++ atualizada/02.js
@@ -0,0 +1,8 @@
+<script>
+const element = document.getElementById("myBtn");
+element.addEventListener("click", myFunction);
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Hello World";
+}
+</script>
